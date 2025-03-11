45b520f6018256a47bf4782247611aa0cd3245e8
jfeng6 <159725684+jfeng6@users.noreply.github.com>
2025-03-11 01:57:36 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
 import os
 from datetime import datetime
-import getpass
 
 # Please customize the following variables
 USER_NAME = "zfeng6"
 VERSION = "1.0.0"
 
-def say_hi(msg:str = "Hi!", file_directory:str = "/app/data/") -> None:
+def say_hi(msg:str = "Hi!", file_directory:str = "./output/") -> None:
+    # Ensure the output directory exists
+    os.makedirs(file_directory, exist_ok=True)
+    
     # Generate timestamp
     timestamp = datetime.now().strftime("%Y%m%d%H%M")
 
     # Define filename with timestamp
     file_name = f"outputfile_{USER_NAME}_{VERSION}_timestamp_{timestamp}.txt"
     file_path = os.path.join(file_directory, file_name)
-
-    # Ensure directory exists
-    os.makedirs(file_directory, exist_ok=True)
 
     # Write the timestamp inside the file
     with open(file_path, "w") as file:
