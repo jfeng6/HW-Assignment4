91a75e0aaef6fde38a4e0abf1b288ce0f7e8af78
jfeng6 <159725684+jfeng6@users.noreply.github.com>
2025-03-11 03:40:34 -0700

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,10 @@
-# Run the tests with: pytest tests/test_app.py
+"""
+Unit tests for app.py
+"""
+
 from ..app import add_numbers
 
 def test_add_numbers():
+    """Test for the add_numbers function"""
     assert 3 == add_numbers(1,2)
     
